78c44a0b0ce900165de12b677e6fce93cc3f3946
obgniyum <muyingbo@risenb.com>
2017-12-14 16:49:36 +0800

    create

--- do.sh
+++ do.sh
@@ -1,8 +1,8 @@
 
 
-###### verify git remote
-firstPush=false
+
 printf "\nStep1: remote verifying ...\n"
+firstPush=false
 result_gitRemote=`git remote`
 if [ -z "$result_gitRemote" ];
 then
@@ -19,64 +19,56 @@ echo "✅✅✅ remote verify success"
 fi
 
 
-
-if $firstPush
+printf "\nStep2: pod lib lint ...\n"
+result_podLibLint=`pod lib lint --allow-warnings`
+#var1=`pod lib lint --sources='https://gitee.com/jiqirenapp/JQRPods.git','https://gitee.com/hongruisibo_iOS/hrpods.git','https://github.com/CocoaPods/Specs.git' --allow-warnings
+echo $result_podLibLint |grep -q "passed validation"
+if [ $? -eq 0 ]
 then
-#git push -u origin master
-echo "is first"
+echo "✅✅✅ pod lib lint success"
 else
-#git push
-echo "not first"
+echo "❌❌❌ pod lib lint failure"
+exit
 fi
 
 
-#printf "\nStep2: pod lib lint ...\n"
-#result_podLibLint=`pod lib lint --allow-warnings`
-##var1=`pod lib lint --sources='https://gitee.com/jiqirenapp/JQRPods.git','https://gitee.com/hongruisibo_iOS/hrpods.git','https://github.com/CocoaPods/Specs.git' --allow-warnings
-#echo $result_podLibLint |grep -q "passed validation"
-#if [ $? -eq 0 ]
-#then
-#echo "✅✅✅ pod lib lint success"
-#else
-#echo "❌❌❌ pod lib lint failure"
-#exit
-#fi
-#
-#
-#printf "\nStep3: push code to git ...\n"
-#read -p "input commit log:" var_commitLog
-#git add .
-#git commit -m "${var_commitLog}"
-#if $firstPush
-#then
-##git push -u origin master
-#echo "is first"
-#else
-##git push
-#echo "not first"
-#fi
+printf "\nStep3: push code to git ...\n"
+read -p "input commit log: " var_commitLog
+git add .
+git commit -m "${var_commitLog}"
+if $firstPush
+then
+git push -u origin master
+else
+git push
+fi
+echo "✅✅✅ push code complete"
 
 
-
-#read -p "input commit tag(url):" var_commitTag
-#git tag $var_commitTag
-#git push --tags
-
-
-
+printf "\nStep4: push tag to git ...\n"
+read -p "input commit tag: " var_commitTag
+git tag $var_commitTag
+git push --tags
+echo "✅✅✅ push tag complete"
 
 
+printf "\nStep5: pod spec lint ...\n"
+result_podLibLint=`pod spec lint --allow-warnings`
+#var1=`pod lib lint --sources='https://gitee.com/jiqirenapp/JQRPods.git','https://gitee.com/hongruisibo_iOS/hrpods.git','https://github.com/CocoaPods/Specs.git' --allow-warnings
+echo $result_podSpecLint |grep -q "passed validation"
+if [ $? -eq 0 ]
+then
+echo "✅✅✅ pod spec lint success"
+else
+echo "❌❌❌ pod spec lint failure"
+exit
+fi
 
 
-#
-#echo "开始发布(验证并添加到本地仓库)..."
-#pod repo push JQRPods --allow-warnings
-#echo "✅  发布成功 -> ${var_tag}"
-#
-#else
-#echo "❌ 本地验证失败"
-#echo ${var1}
-#fi
+printf "\nStep6: pod trunk push ...\n"
+pod repo push JQRPods --allow-warnings
+echo "✅✅✅  pod trunk push complete -> ${var_commitTag}"
+
 
 
 
