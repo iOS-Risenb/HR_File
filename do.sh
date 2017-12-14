92eea047a373ae87f843b9a084eead117c1d989c
obgniyum <muyingbo@risenb.com>
2017-12-14 16:39:59 +0800

    adad

--- do.sh
+++ do.sh
@@ -1,7 +1,7 @@
 
 
 ###### verify git remote
-firstPush = false
+firstPush=false
 printf "\nStep1: remote verifying ...\n"
 result_gitRemote=`git remote`
 if [ -z "$result_gitRemote" ];
@@ -13,29 +13,13 @@ git remote add origin $var_remoteUrl
 echo "✅✅✅ add remote success"
 echo "remote name is 'origin'"
 echo "remote url is '$var_remoteUrl'"
-firstPush = true
+firstPush=true
 else
 echo "✅✅✅ remote verify success"
 fi
 
 
-printf "\nStep2: pod lib lint ...\n"
-result_podLibLint=`pod lib lint --allow-warnings`
-#var1=`pod lib lint --sources='https://gitee.com/jiqirenapp/JQRPods.git','https://gitee.com/hongruisibo_iOS/hrpods.git','https://github.com/CocoaPods/Specs.git' --allow-warnings
-echo $result_podLibLint |grep -q "passed validation"
-if [ $? -eq 0 ]
-then
-echo "✅✅✅ pod lib lint success"
-else
-echo "❌❌❌ pod lib lint failure"
-exit
-fi
 
-
-printf "\nStep3: push code to git ...\n"
-read -p "input commit log:" var_commitLog
-git add .
-git commit -m "${var_commitLog}"
 if $firstPush
 then
 #git push -u origin master
@@ -45,9 +29,41 @@ else
 echo "not first"
 fi
 
+
+#printf "\nStep2: pod lib lint ...\n"
+#result_podLibLint=`pod lib lint --allow-warnings`
+##var1=`pod lib lint --sources='https://gitee.com/jiqirenapp/JQRPods.git','https://gitee.com/hongruisibo_iOS/hrpods.git','https://github.com/CocoaPods/Specs.git' --allow-warnings
+#echo $result_podLibLint |grep -q "passed validation"
+#if [ $? -eq 0 ]
+#then
+#echo "✅✅✅ pod lib lint success"
+#else
+#echo "❌❌❌ pod lib lint failure"
+#exit
+#fi
+#
+#
+#printf "\nStep3: push code to git ...\n"
+#read -p "input commit log:" var_commitLog
+#git add .
+#git commit -m "${var_commitLog}"
+#if $firstPush
+#then
+##git push -u origin master
+#echo "is first"
+#else
+##git push
+#echo "not first"
+#fi
+
+
+
 #read -p "input commit tag(url):" var_commitTag
 #git tag $var_commitTag
 #git push --tags
+
+
+
 
 
 
